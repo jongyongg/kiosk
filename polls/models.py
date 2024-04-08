0c3fe19db0faf3ab8cab46798d46e2923ec8fcc9
jongyongg <leejong7492@naver.com>
2024-04-08 16:21:38 +0900

    a

--- polls/models.py
+++ polls/models.py
@@ -35,3 +35,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import F
 from .models import Question, Choice, Order, Product
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,52 +18,43 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
+@login_required
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        return render(
-            request,
-            "polls/detail.html",
-            {"question": question, "error_message": "You didn't select a choice."},
-        )
-    else:
-        selected_choice.votes = F("votes") + 1
-        selected_choice.save()
+    
+    # Create an order only if the user is authenticated
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    selected_choice.votes += 1
+    selected_choice.save()
+   
+     #else:
+       
+        # Create an order only if the user is authenticated
+       # if request.user.is_authenticated:
+       #     quantity = 1  # Assuming quantity is always 1 for a vote
+       #     price = 0  # You might need to set the price
+    #         total_price = price * quantity
+    #         Order.objects.create(question=question, user=request.user, quantity=quantity, price=price, total_price=total_price)
+    #     return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-        # 주문을 생성합니다.
-        quantity = int(request.POST.get('quantity'))  # 주문 수량
-        price = selected_choice.price  # 선택된 항목의 가격
-        total_price = quantity * price  # 해당 주문의 총 가격
-
-        # 주문 정보를 저장합니다.
-        order = Order.objects.create(question=question, quantity=quantity, price=price, total_price=total_price, user=request.user)
-
-        # 주문 확인 페이지로 리다이렉트합니다.
-        return redirect('polls:confirm_order')
-
+@login_required
 def order(request):
     if request.method == 'POST':
         product_id = request.POST.get('product_id')
         quantity = int(request.POST.get('quantity'))
-        product = Product.objects.get(pk=product_id)
+        product = get_object_or_404(Product, pk=product_id)
         total_price = product.price * quantity
         # 주문을 생성합니다.
-        Order.objects.create(question=None, user=request.user, quantity=quantity, price=product.price, total_price=total_price)
+        Order.objects.create(product=product, user=request.user, quantity=quantity, price=product.price, total_price=total_price)
         return redirect('polls:confirm_order')
     else:
         products = Product.objects.all()
         return render(request, 'polls/order.html', {'products': products})
 
+@login_required
 def confirm_order(request):
-    if not request.user.is_authenticated:
-        # 사용자가 로그인되어 있지 않으면 로그인 페이지로 리다이렉트합니다.
-        return redirect('login')
-
     # 현재 로그인된 사용자의 주문 목록을 가져옵니다.
     orders = Order.objects.filter(user=request.user)
     # 총 가격을 계산합니다.
     total_price_all = sum(order.total_price for order in orders)
     return render(request, 'polls/confirm_order.html', {'orders': orders, 'total_price_all': total_price_all})
-
